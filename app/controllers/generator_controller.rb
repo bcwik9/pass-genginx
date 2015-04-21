4c074a6d35cdde5311656fe1350491b77468ebf6
Ubuntu <ubuntu@ip-172-31-37-57.ec2.internal>
2015-04-21 00:47:43 +0000

    add examples dir to cloudform lib. fix bug in parameter where default wasnt being set correctly. add functionality to easily add options to parameters

--- app/controllers/generator_controller.rb
+++ app/controllers/generator_controller.rb
@@ -92,27 +92,37 @@ class GeneratorController < ApplicationController
   end
 
   def eb_app_call
-    eb_app = AwsElasticBeanstalkApplication.new
+secret_key_param = AwsParameter.new(:logical_id => "SecretKeyBase", :description => "Rails secret key base for production", :default => "CHANGEME")
+  secret_key_param.add_option(:minLength, 1)
 
-    eb_version = AwsElasticBeanstalkApplicationVersion.new
-    eb_version.set_application_name eb_app.get_reference
-    eb_version.set_source 'elasticbeanstalk-us-east-1-719719598906', 'questionnaire-forms-master.zip'
+  bucket_name_param = AwsParameter.new(:logical_id => "BucketName", :description => "S3 Bucket name where Rails project is stored")
+  bucket_name_param.add_option(:minLength, 1)
 
-    eb_config = AwsElasticBeanstalkConfigurationTemplate.new
-    eb_config.set_application_name eb_app.get_reference
-    eb_config.enable_single_instance
-    #eb_config.set_image_id 'ami-26b9834e'
-    eb_config.set_instance_type 't2.micro'
-    eb_config.set_stack_name "64bit Amazon Linux 2015.03 v1.3.0 running Ruby 2.2 (Passenger Standalone)"
+  project_zip_param = AwsParameter.new(:logical_id => "ProjectZip", :description => "Rails project zip file")
+  project_zip_param.add_option(:minLength, 1)
+  
+  eb_app = AwsElasticBeanstalkApplication.new
 
-    eb_env = AwsElasticBeanstalkEnvironment.new
-    eb_env.set_application_name eb_app.get_reference
-    eb_env.set_template_name eb_config.get_reference
-    eb_env.set_version_label eb_version.get_reference
-    
-    # create a blank template and add all the resources we need
-    template = AwsTemplate.new
-    template.add_resources [eb_app, eb_version, eb_config, eb_env]
+  eb_version = AwsElasticBeanstalkApplicationVersion.new
+  eb_version.set_application_name eb_app.get_reference
+  eb_version.set_source bucket_name_param.get_reference, project_zip_param.get_reference
+
+  eb_config = AwsElasticBeanstalkConfigurationTemplate.new
+  eb_config.set_application_name eb_app.get_reference
+  eb_config.enable_single_instance
+  eb_config.set_instance_type 't2.micro'
+  eb_config.set_stack_name "64bit Amazon Linux 2015.03 v1.3.0 running Ruby 2.2 (Passenger Standalone)"
+  eb_config.set_option "aws:elasticbeanstalk:application:environment", "SECRET_KEY_BASE", secret_key_param.get_reference
+
+  eb_env = AwsElasticBeanstalkEnvironment.new
+  eb_env.set_application_name eb_app.get_reference
+  eb_env.set_template_name eb_config.get_reference
+  eb_env.set_version_label eb_version.get_reference
+  
+  # create a blank template and add all the resources we need
+  template = AwsTemplate.new
+  template.add_resources [eb_app, eb_version, eb_config, eb_env]
+  template.add_parameters [secret_key_param, bucket_name_param, project_zip_param]
 
     render :json => template.to_json
   end
--- lib/cloudform/parameter.rb
+++ lib/cloudform/parameter.rb
@@ -3,7 +3,7 @@ require_relative 'aws_object'
 class AwsParameter
   include AwsObject
 
-  attr_accessor :description, :type, :allowed_values, :default, :other
+  attr_accessor :description, :type, :allowed_values, :default, :options
   
   ALLOWED_TYPES = ["String", "AWS::EC2::KeyPair::KeyName"]
 
@@ -14,18 +14,38 @@ class AwsParameter
     raise "Unsupported type: #{@type}" unless ALLOWED_TYPES.include? @type
     @allowed_values = opt[:allowed_values] || []
     @default = opt[:default]
-    @other = opt[:other] || {}
+    @options = opt[:options] || {}
+  end
+
+  def add_option key, value
+    raise "Must specify valid key and value" if key.nil? or key.empty? or value.nil?
+    @options[key] = value
   end
     
   def to_h
     ret = super
     ret[@logical_id][:Description] = @description
     ret[@logical_id][:Type] = @type
-    ret[@logical_id][:Default] if @default
+    ret[@logical_id][:Default] = @default if @default
     ret[@logical_id][:AllowedValues] = @allowed_values unless @allowed_values.empty?
-    # iterate through all other keys and add them
-    @other.each do |k,v|
+    # iterate through all options keys and add them
+    capitalize_keys(@options).each do |k,v|
       ret[@logical_id][k] = v
+    end
+
+    return ret
+  end
+
+  private
+
+  # capitalize all keys in a hash if necessary
+  def capitalize_keys h
+    ret = {}
+
+    h.each do |k,v|
+      new_key = k.to_s.split('')
+      new_key[0].upcase!
+      ret[new_key.join] = v
     end
 
     return ret
