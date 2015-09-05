f441dff5d9e9710b5c98e9613ffb33078710e7b9
Ubuntu <ubuntu@ip-172-31-37-57.ec2.internal>
2015-09-05 00:10:16 +0000

    assign public IPs from subnet by default

--- lib/cloudform/subnet_resource.rb
+++ lib/cloudform/subnet_resource.rb
@@ -12,6 +12,7 @@ class AwsSubnet
     @cidr_block = opt[:cidr_block] || '10.0.0.0/24'
     @vpc = opt[:vpc]
     add_property :AvailabilityZone, opt[:availability_zone] if opt[:availability_zone]
+    add_property :MapPublicIpOnLaunch, (opt[:assign_public_ips] || true)
   end
 
   def set_vpc vpc
